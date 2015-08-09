eddef1cccd68068d007084e55ca9f35dc8d8b409
Christoph Grabo <asaaki@mannaz.cc>
2015-08-09 06:17:35 +0200

    Remove EDIP tool data
    The EDIP tool is a standalone Docker image now.
    See: <https://github.com/asaaki/docker-images/tree/master/dockerfiles/edip-tool/0.3.0>

--- tools/app_info.exs
+++
@@ -1,9 +0,0 @@
-defmodule AppInfo do
-  def name,    do: config |> Dict.get(:app)
-  def version, do: config |> Dict.get(:version)
-  def exrm,    do: deps |> Dict.has_key?(:exrm)
-  def phoenix, do: deps |> Dict.has_key?(:phoenix)
-
-  defp config, do: Mix.Project.config
-  defp deps,   do: config |> Dict.get(:deps)
-end
